e3864d035cf15bb9c4d566f1a90765c2b8b0c2a6
ankesai6 <96909598+ankesai6@users.noreply.github.com>
2022-02-05 11:08:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Skills from "./components/skills/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/Resume";
@@ -31,6 +32,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/project" component={Projects} />
           <Route path="/about" component={About} />
+          <Route path="/skills" component={Skills} />
           <Route path="/resume" component={Resume} />
         </Switch>
         <Footer />
