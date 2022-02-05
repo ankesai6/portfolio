57d17071d987a7aac4691be4cb9cd612bd4fe640
ankesai6 <96909598+ankesai6@users.noreply.github.com>
2022-02-05 13:09:54 +0530

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,43 +1,105 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
 import Resumecontent from "./ResumeContent";
 import ResumePdf from "../../Assets/Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 
-function Resume() {
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {AiFillGithub,AiOutlineTwitter,} from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
+import Particle from "../Particle";
+
+
+function Home2() {
   return (
-    <Container fluid className="resume-section">
+    <Container fluid className="home-about-section" id="about">
       <Particle />
       <Container>
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={ResumePdf} target="_blank">
-            <AiOutlineDownload />
-            &nbsp;Resume
-          </Button>
-        </Row>
-        <Row className="resume">
-           <Col md={6} className="resume-right">
-            <h3 className="resume-title">Education</h3>
-            <Resumecontent
-              title="B.Tech in Computer Science Engineering [SJCIT, Chickballapur, Banglore] "
-              date="2016 - 2020"
-              content={[`CGPA: 6.2`]}
-            />
-            <Resumecontent
-              title="12th BOARD [Sri Chaitanya Junior College, Vijayawada]"
-              date="2014 - 2016"
-              content={["Percentage: 87.4%"]}
-            />
-            <Resumecontent
-              title="10th BOARD [Prasad Concept School, Anantapur] "
-              date="2014"
-              content={[`CGPA: 8.8%`]}
-            />
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I'm Programmer that turns coffee into code.😉
+              <br />
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple">
+                  {" "}
+                  Javascript and MERN stack.{" "}
+                </b>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are building new &nbsp;
+              <i>
+                <b className="purple">Web Technologies and Products </b>
+               
+              </i>
+              <br />
+              <br />
+              Whenever possible, I also apply my passion for developing products
+              with
+              <i>
+                <b className="purple"> Modern Javascript Frameworks</b>
+              </i>
+              &nbsp; like
+              <i>
+                <b className="purple"> Node.js and React.js</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
           </Col>
         </Row>
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>Contact</h1>
+            <p>
+              Feel free to <span className="purple">Connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              
+              <li className="social-icons">
+                <a
+                  href="https://github.com/ankesai8"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+           <li className="social-icons">
+                <a
+                  href="mailto:ankesai8@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineMail />
+                </a>
+              </li>
+            
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/saiprasad_264"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button variant="primary" href={ResumePdf} target="_blank">
             <AiOutlineDownload />
             &nbsp;Resume
@@ -47,5 +109,29 @@ function Resume() {
     </Container>
   );
 }
+export default Home2;
+       
 
-export default Resume;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
