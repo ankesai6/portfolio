0238a070f62a72d441da40ffabccf33ce04ed4a2
ankesai6 <96909598+ankesai6@users.noreply.github.com>
2022-02-05 11:58:06 +0530

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,51 +1,63 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
-import Resumecontent from "./ResumeContent";
-import ResumePdf from "../../Assets/Resume.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
 
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,} from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
+import Particle from "../Particle";
 function Resume() {
-  return (
-    <Container fluid className="resume-section">
+      <Container fluid className="home-about-section" id="about">
       <Particle />
       <Container>
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={ResumePdf} target="_blank">
-            <AiOutlineDownload />
-            &nbsp;Resume
-          </Button>
-        </Row>
-        <Row className="resume">
-           <Col md={6} className="resume-right">
-            <h3 className="resume-title">Education</h3>
-            <Resumecontent
-              title="B.Tech in Computer Science Engineering [SJCIT, Chickballapur, Banglore] "
-              date="2016 - 2020"
-              content={[`CGPA: 6.2`]}
-            />
-            <Resumecontent
-              title="12th BOARD [Sri Chaitanya Junior College, Vijayawada]"
-              date="2014 - 2016"
-              content={["Percentage: 87.4%"]}
-            />
-            <Resumecontent
-              title="10th BOARD [Prasad Concept School, Anantapur] "
-              date="2014"
-              content={[`CGPA: 8.8%`]}
-            />
+        
+         <Row>
+          <Col md={12} className="home-about-social">
+            <h1>Contact</h1>
+            <p>
+              Feel free to <span className="purple">Connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              
+              <li className="social-icons">
+                <a
+                  href="https://github.com/ankesai8"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+           <li className="social-icons">
+                <a
+                  href="mailto:ankesai8@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineMail />
+                </a>
+              </li>
+            
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/saiprasad_264"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+            </ul>
           </Col>
-        </Row>
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={ResumePdf} target="_blank">
-            <AiOutlineDownload />
-            &nbsp;Resume
-          </Button>
         </Row>
       </Container>
     </Container>
   );
+  
 }
 
 export default Resume;
