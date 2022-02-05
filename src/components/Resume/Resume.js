6929075afb3f8a5c631956a5f77fbe69eb69ebfb
ankesai6 <96909598+ankesai6@users.noreply.github.com>
2022-02-05 13:24:51 +0530

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,21 +3,20 @@ import { AiOutlineDownload } from "react-icons/ai";
 
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-
-
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
 import {AiFillGithub,AiOutlineTwitter,} from "react-icons/ai";
 import { AiOutlineMail } from "react-icons/ai";
 import Particle from "../Particle";
 import Button from "react-bootstrap/Button";
 
 
-function Resume() {
+function Home2() {
   return (
     <Container fluid className="home-about-section" id="about">
       <Particle />
       <Container>
-
-        <Row>
+     <Row>
           <Col md={12} className="home-about-social">
             <h1>Contact</h1>
             <p>
@@ -65,33 +64,52 @@ function Resume() {
             &nbsp;Resume
           </Button>
         </Row>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              <span className="purple"></span> 
+            </h1>
+            <p className="home-about-body">
+              I'm Programmer that turns coffee into code.😉
+              <br />
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple">
+                  {" "}
+                  Javascript and MERN stack.{" "}
+                </b>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are building new &nbsp;
+              <i>
+                <b className="purple">Web Technologies and Products </b>
+               
+              </i>
+              <br />
+              <br />
+              Whenever possible, I also apply my passion for developing products
+              with
+              <i>
+                <b className="purple"> Modern Javascript Frameworks</b>
+              </i>
+              &nbsp; like
+              <i>
+                <b className="purple"> Node.js and React.js</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+       
       </Container>
     </Container>
   );
 }
-export default Resume;
+export default Home2;
        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
      
