f3087a3d30421d83f88fb08df684fbc62c531574
ankesai6 <96909598+ankesai6@users.noreply.github.com>
2022-02-05 11:48:40 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -26,7 +26,7 @@ function Home() {
                 <strong className="main-name"> ANKE SAIPRASAD</strong>
               </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
+              <div style={{ padding: 25, textAlign: "left" }}>
                 <Type />
               </div>
          
